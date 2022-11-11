cc560913a4520483951678fe48545f1e907e0fb5
zeemarquez <zeemarquezamazon@gmail.com>
2022-11-11 09:21:53 +0100

    mesh marc

--- helpers.py
+++ helpers.py
@@ -79,3 +79,42 @@ else:
     
 
 #%%
+import matplotlib.pyplot as plt
+
+logpath = r'/Users/zeemarquez/openfoam/assignment/groundeffect/case/log.csv'
+
+logdic = {}
+f = open(logpath,'r')
+text = f.read()
+f.close()
+lines= text.split('\n')
+headers = lines[0].split(';')
+
+for header in headers:
+    logdic[header] = []
+
+for line in lines[1:]:
+    cols = line.split(';')
+    
+    for col, key in zip(cols,logdic.keys()):
+        logdic[key].append(col)
+
+logdic['T'] = logdic['T'][:-1]
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+    
+ax.clear()
+for key in logdic.keys():
+    if key != 'T':
+        ax.plot(logdic['T'],logdic[key], label = key)
+
+plt.yscale('log')
+plt.grid(which='both')
+plt.title('Residuals')
+plt.legend(loc='upper right')
+fig.set_size_inches(10, 6)
+fig.tight_layout()
+plt.show()
+# %%
